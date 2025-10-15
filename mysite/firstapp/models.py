a6241353d122ec8fbdbf3b2999d94281c331216b
unknown <Student@T1-PC7.StepIT.Prg.local>
2025-10-15 17:42:05 +0200

    databaze

--- mysite/firstapp/models.py
+++ mysite/firstapp/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    name = models.CharField(max_length=100)
+    text = models.TextField(null=True, blank=True)
